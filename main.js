1e781c5b801264cac771b511b92430db832c5882
Rawle Juglal <rawledjuglal@gmail.com>
2023-02-01 19:14:39 -0600

    deployed app
    finished blackjack and deployed with ghpages

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 import './style.css'
-document.querySelector('#app').innerHTML = `
-  <div id="game-container">
-    <h1>Blackjack</h1>
-    <p id="message-el">Want to play a round?</p>
-    <p id="cards-el">Cards:</p>
-    <p id="sum-el">Sum:</p>
-    <p id="player-el"></p>
-    <button id="start">START GAME</button>
-    <button id="newCard">New Card</button>
-  </div>
-`
 
-let cards = []
+let isLoading = true;
+let deck;
+let cards;
+fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+  .then(res=>res.json())
+  .then(data=>{
+    isLoading = false;
+    deck = data;
+    return data;
+  })
+
 let player = {
   name:"Rawle",
   chips:145
@@ -31,27 +30,63 @@ function setRandomNum(){
   return Math.floor(Math.random() * 10) + 2;
 }
 
+function checkForValue(val){
+  // console.log(`entered check`)
+  console.log(val);
+  if(val === 'ACE'){
+    return 11;
+  } else if(val === 'JACK' || val === 'QUEEN' || val === 'KING'){
+    return 10;
+  } else {
+    return Number(val)
+  }
+}
+
 function startGame(){
   isAlive = true;
-  let firstCard = setRandomNum();
-  let secondCard = setRandomNum();
-  cards = [firstCard, secondCard];
-  sum = firstCard + secondCard;
-  renderGame();
+  player.chips -= 5;
+  playerEl.textContent = `${player.name}: $${player.chips}`
+  if(document.getElementsByClassName('card-img').length > 0){
+    document.getElementById('cards-container').innerHTML = ''
+  }
+  
+  cards = [];
+
+  if(!isLoading){
+    isLoading = true;
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=2`)
+      .then(res=>res.json())
+      .then(data=>{
+        cards = data;
+        sum = checkForValue(data.cards[0].value) + checkForValue(data.cards[1].value);
+        renderImages(data.cards)
+        isLoading = false;
+        renderGame();
+        return data;
+      })
+  }
+}
+
+function renderImages(cardArr){
+  cardArr.map((item)=>{
+    // console.log(item)
+    let cardImg = document.createElement('img')
+    cardImg.setAttribute('id', item.code)
+    cardImg.classList.add('card-img')
+    cardImg.src = item.image
+    document.getElementById('cards-container').appendChild(cardImg);
+  })
 }
 
 function renderGame(){
-  console.log('clicked start game')
-  sumEl.textContent = "Sum: " + sum;
-  cardEl.textContent = "Cards: "
-  cards.map((item)=>{
-    cardEl.textContent += " " + item
-  })
+  sumEl.textContent = "Sum :" + sum
   if (sum <= 20) {
       message = "Do you want to draw a new card?"
   } else if (sum === 21) {
       message = "You've got Blackjack!"
       hasBlackJack = true
+      player.chips += 10;
+      playerEl.textContent = `${player.name}: $${player.chips}`
   } else {
       message = "You're out of the game!"
       isAlive = false
@@ -61,13 +96,21 @@ function renderGame(){
 }
 
 function newCard(){
-  console.log('Drawing a new card from the deck!')
-  if(isAlive && !hasBlackJack){
-    let newCard = setRandomNum();
-    console.log(newCard)
-    cards.push(newCard);
-    sum += newCard;
-    renderGame()
+  // console.log('Drawing a new card from the deck!')
+  if(isAlive && !hasBlackJack && !isLoading){
+    isLoading = true;
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=1`)
+      .then(res=>res.json())
+      .then(data=>{
+        // console.log(data)
+        cards.cards.push(data.cards[0])
+        cards.remaining = data.remaining;
+        sum += checkForValue(data.cards[0].value)
+        renderImages(data.cards)
+        isLoading = false;
+        renderGame()
+        return data;
+      })
   }
   
 }
